67e28a349c07773bfa273264ca5d48ab92c4c418
David Damke <dd-192057@rwu.de>
2022-12-29 11:33:00 +0100

    add simple navgation only style not working yet

--- portfoliowebapp/src/App.vue
+++ portfoliowebapp/src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <button @click="changePage"> MainPage</button>
   <MainPage></MainPage>
 </template>
 
@@ -23,12 +22,7 @@ methods: {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  background-color: black;
 }
 </style>
--- portfoliowebapp/src/components/MainPage.vue
+++ portfoliowebapp/src/components/MainPage.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
-    <text>{{testText}}</text>
-    <text> HO </text>
+  <div class="mainPage">
+    <div class="menu">
+      <div class="item"> 
+        <div class="itemText">Projects </div>
+      </div> 
+      <div class="item" >
+        <div class="itemText">About me</div>
+       </div> 
+      <div class="item" >  
+        <div class="itemText">CV</div>
+    </div>
+    </div>
+  
     
   </div>
 </template>
@@ -25,5 +35,38 @@ export default {
 </script>
 
 <style>
+
+.item{
+  flex: 1;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.itemText:hover{
+  color:lightblue;
+  border:2px solid blue; 
+}
+.itemText{
+  font-size: 20px;
+  color: #818181;
+  cursor: pointer;
+  padding: 10px;
+  border-bottom: 2px solid #080808;
+  width: fit-content;
+}
+
+.menu{
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin-top: 10px;
+  height: 60px;
+}
+.mainPage{
+  vertical-align: center;
+  align-items: center;
+}
 
 </style>
